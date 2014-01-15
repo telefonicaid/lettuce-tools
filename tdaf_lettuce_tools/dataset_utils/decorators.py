1a2734b3668fd84b02aef305b3e2c16ecdc52316
Roberto Afonso <ran@tid.es>
2014-01-15 12:20:09 +0100

    Applied pep8 Improved implementation

--- tdaf_lettuce_tools/dataset_utils/decorators.py
+++ tdaf_lettuce_tools/dataset_utils/decorators.py
@@ -12,28 +12,26 @@ been supplied.
 from functools import wraps
 from dataset_utils import DatasetUtils
 
+
 def auto_expand(f):
+    '''Automation of the detection and execution of the fixed length pattern'''
     @wraps(f)
     def wrapper(*args, **kwargs):
         dataset_utils = DatasetUtils()
-        new_args = []
-        for arg in args:
-            new_args.append(dataset_utils.generate_fixed_length_param(arg))
-                            
+        args = map(dataset_utils.generate_fixed_length_param, args)
         kwargs.update(dataset_utils.generate_fixed_length_params(kwargs))
-            
-        return f(*new_args, **kwargs)
+        return f(*args, **kwargs)
     return wrapper
 
 
-
 @auto_expand
-def prueba(arg1, arg2, arg3):
+def test(arg1, arg2, arg3):
     print 'arg1', arg1
     print 'arg2', arg2
     print 'arg3', arg3
 
 
 if __name__ == '__main__':
-    prueba('[STRING_WITH_LENGTH_5]', arg3 = 'standard chain', arg2 = '[STRING_WITH_LENGTH_10]')
-
+    test('[STRING_WITH_LENGTH_5]',
+         arg3='standard chain',
+         arg2='[STRING_WITH_LENGTH_10]')
